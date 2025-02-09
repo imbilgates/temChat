7b27cd25e0594188a826ea487dd870c38d3367c1
bilgates27 <bilgates270703@gmail.com>
2025-02-09 22:50:42 +0530

    refactor: migrate App and SocketContext to new file structure; update index to use new components

--- server/server.js
+++ server/server.js
@@ -84,7 +84,7 @@ io.on('connection', (socket) => {
 
 // Serve the client app
 app.use(express.static(path.join(__dirname, '../client', 'build')));
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
 
