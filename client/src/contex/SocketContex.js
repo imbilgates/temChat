c327f393d488f16f25dac1e72722f53e6413070f
bilgates27 <bilgates270703@gmail.com>
2025-02-09 22:03:35 +0530

    update asset manifest and HTML for new build files; change socket connection to production URL

--- client/src/contex/SocketContex.js
+++ client/src/contex/SocketContex.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 const SocketContext = createContext();
 
 const SocketProvider = ({ children }) => {
-    const socket = useMemo(() => io("http://localhost:5000"), []);
+    const socket = useMemo(() => io("https://tempchat-dn7j.onrender.com"), []);
 
     useEffect(() => {
         return () => {
